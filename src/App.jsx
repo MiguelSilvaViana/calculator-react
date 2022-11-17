85838dff4be65012578f087701553d4fe597ea94
Miguel <miguelsilvavianaa@gmail.com>
2022-11-17 15:11:18 -0300

    tail-react

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import styles from "./index.css"
+import React from "react"
+
+export default () => 
+    <div id="App">
+        <h1 className="bg-zinc-300">
+            Olá mundo
+        </h1>
+    </div>
